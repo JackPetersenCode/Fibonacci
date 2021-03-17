0b90e3b95f6699b4a6ecd24dd2b6aea050d58a59
CheckOutJacksCode <79170489+CheckOutJacksCode@users.noreply.github.com>
2021-03-16 18:24:03 -0700

    Add files via upload

--- fibonacci.py
+++ fibonacci.py
@@ -1,9 +1,9 @@
-def fibonacci(upper_limit):
+def fibonacci(upper_limit, lower_limit):
     fib = [0, 1]
     if upper_limit == 0:
         return 0
     if upper_limit == 1:
-        return 1
+        return 0, 1
     else:
         i = 1
         while fib[i] < upper_limit:
@@ -12,13 +12,20 @@ def fibonacci(upper_limit):
                 fib.append(next_fib)
                 i += 1
             else:
-                return fib
+                j = 0
+                while lower_limit > fib[j]:
+                    j += 1
+                return fib[j:]
+
+                #return fib
+
 go_again_letter = "Y"
 while go_again_letter != "N":
     user_input = int(input("What is the upper limit? "))
-    print(fibonacci(user_input))
+    user_lower = int(input("What is the lower limit? "))
+    print(fibonacci(user_input, user_lower))
+    #print(add_lower_limit(upper_fib_list, user_lower))
     go_again_letter = input("Want to go again? Y for yes and N for no: ")
     possible_options = ["Y", "N"]
     while go_again_letter not in possible_options:
         go_again_letter = input("Give me a Y or N: ")
-
